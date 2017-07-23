9306570b3a2bc1708b9ffdcb59d7eee6dabd1d2a
Dilun7525 <Freeworknet@yandex.ru>
2017-07-23 22:34:06 +0300

    т

--- app/src/main/java/com/example/android/justjava/MainActivity.java
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -36,8 +36,6 @@ public class MainActivity extends AppCompatActivity {
     /**
      * This method displays the given quantity value on the screen.
      */
-
-
     private void display(int number) {
         TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
         quantityTextView.setText(String.valueOf(number));
@@ -49,7 +47,7 @@ public class MainActivity extends AppCompatActivity {
 
     private void displayMessage() {
         int number = quantity * priceOfCoffee;
-        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
+        TextView priceTextView = (TextView) findViewById(R.id.order_summary_text_view);
         String priceTotal;
         priceTotal = (NumberFormat.getCurrencyInstance().format(number));
         String priceMessage =
