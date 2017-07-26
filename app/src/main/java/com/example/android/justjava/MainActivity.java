dfa7595742124f8e3eafa84912f7dd23ef7f9fa7
Dilun7525 <Freeworknet@yandex.ru>
2017-07-26 22:24:16 +0300

    Чекбоксы влияют на итоговый чек и на сумму заказа

--- app/src/main/java/com/example/android/justjava/MainActivity.java
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.android.justjava;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.CheckBox;
 import android.widget.TextView;
 
 import java.text.NumberFormat;
@@ -11,8 +12,14 @@ import java.text.NumberFormat;
  * This app displays an order form to order coffee.
  */
 public class MainActivity extends AppCompatActivity {
+
+    /*Переменные: цены, количество по умолчанию*/
     protected int quantity = 1;
     protected int priceOfCoffee = 120;
+    protected int toppingCream = 30;
+    protected int toppingMarshmallow = 45;
+    protected int toppingSyrop = 50;
+    protected int toppingPriceTotal = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,14 +52,38 @@ public class MainActivity extends AppCompatActivity {
         displayMessage();
     }
 
+    protected String toppingMessage() {
+        String toppings = "";
+        toppingPriceTotal = 0;
+
+        CheckBox checkBoxCrem = (CheckBox) findViewById(R.id.crem);
+        if (checkBoxCrem.isChecked()) {
+            toppings = toppings + "\nAdd Whipped cream: " + toppingCream + " руб.";
+            toppingPriceTotal += toppingCream;
+        }
+        CheckBox checkBoxMarshmallow = (CheckBox) findViewById(R.id.marshmallow);
+        if (checkBoxMarshmallow.isChecked()) {
+            toppings = toppings + "\nAdd Marshmallow: " + toppingMarshmallow + " руб.";
+            toppingPriceTotal += toppingMarshmallow;
+        }
+        CheckBox checkBoxSyrop = (CheckBox) findViewById(R.id.syrop);
+        if (checkBoxSyrop.isChecked()) {
+            toppings = toppings + "\nAdd Syrop: " + toppingSyrop + " руб.";
+            toppingPriceTotal += toppingSyrop;
+        }
+        return toppings;
+    }
+
     private void displayMessage() {
-        int number = quantity * priceOfCoffee;
+        String toppings = toppingMessage();
+        int number = quantity * (priceOfCoffee + toppingPriceTotal);
         TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
-        String priceTotal;
+        String priceTotal = "";
         priceTotal = (NumberFormat.getCurrencyInstance().format(number));
         String priceMessage =
-                        "Name: Kunal Balbeskin" +
+                "Name: Kunal Balbeskin" +
                         "\nQuantity: " + quantity + " coffee" +
+                        toppings +
                         "\nTotal: " + priceTotal +
                         "\n------------------------------" +
                         "\nThank YOU!";
